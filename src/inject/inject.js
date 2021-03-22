7515b38c38c52f5676cbaf05a0c58a447cdf68be
Sergey Ch <mrretoy@gmail.com>
2021-03-22 19:01:59 +0500

    init

---
+++ src/inject/inject.js
@@ -0,0 +1,15 @@
+chrome.extension.sendMessage({}, function (response) {
+  var readyStateCheckInterval = setInterval(function () {
+    if (document.readyState === "complete") {
+      clearInterval(readyStateCheckInterval);
+
+      var css = ".has_nicotine .prdc__img { filter: none; }",
+        head = document.head || document.getElementsByTagName("head")[0],
+        style = document.createElement("style");
+
+      head.appendChild(style);
+
+      style.appendChild(document.createTextNode(css));
+    }
+  }, 10);
+});
